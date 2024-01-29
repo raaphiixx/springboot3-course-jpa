d638908f79a861999d0449c142de9f99546c92ef
Raphaa <raphaatstudy@gmail.com>
2024-01-29 18:59:37 +0000

    Instant, ISO 8601 - PART 1

--- src/main/java/com/educandoweb/course/entites/User.java
+++ src/main/java/com/educandoweb/course/entites/User.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.entites;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
 
     public User() {
@@ -66,6 +71,11 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @OneToMany(mappedBy = "client")
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
