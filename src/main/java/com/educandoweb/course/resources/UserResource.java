398a285c47e0edcc7a23540b43e01887dc13dc65
Raphaa <raphaatstudy@gmail.com>
2024-02-05 17:55:37 +0000

    User Update Method

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -28,17 +28,23 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userServices.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         userServices.delete(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping
-    public ResponseEntity<User> insert(@RequestBody User obj) {
-        obj = userServices.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);
+    @PutMapping (value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = userServices.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
