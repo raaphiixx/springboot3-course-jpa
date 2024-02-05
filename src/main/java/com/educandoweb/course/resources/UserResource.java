4a6f6fcfeb77516c04d86c01ae49d803302e47f8
Raphaa <raphaatstudy@gmail.com>
2024-02-05 12:25:27 +0000

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.educandoweb.course.entites.User;
 import com.educandoweb.course.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +26,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = userServices.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userServices.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
--- src/main/java/com/educandoweb/course/services/UserServices.java
+++ src/main/java/com/educandoweb/course/services/UserServices.java
@@ -21,4 +21,8 @@ public class UserServices {
         Optional<User> obj = userRepository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
 }
